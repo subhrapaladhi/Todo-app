bfa50933c8adc6943c9b4c4a8ca6228536bff9c6
subhrapaladhi <subhrapaladhi9@gmail.com>
2019-10-10 15:10:39 +0530

    connection established

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 let express     = require('express'),
     app         = express(),
-    bodyParser  = require('body-parser');
+    bodyParser  = require('body-parser'),
+    cors        = require('cors');
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,9 +11,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 const publicDir = require('path').join(__dirname,'/../');
 app.use(express.static(publicDir));
 
-app.get('/home', (req,res)=> {
+app.get('/',(req,res)=> {
     res.sendFile(publicDir+'/Frontend/homepage.html')
-    console.log(publicDir);
+})
+
+app.get('/home', (req,res)=> {
+    res.json({
+        taskList:  ['task1','task2','task3','task4','task5']
+    })  
 })
 
 app.listen(3000, ()=>console.log("connected to port 3000"))
--- Frontend/homescript.js
+++ Frontend/homescript.js
@@ -1,6 +1,5 @@
 // let taskList = new Array('task1','task2','task3','task4','task5');
-
-
+let taskList = new Array();
 
 let input = ()=>{
     let newTask = $("form input").val();
@@ -13,19 +12,19 @@ let input = ()=>{
     return false            // the prevent the form from refreshing
 }
 
-let getTaskList = (task)=>{
+let getTaskList = ()=>{
 
-    $.get("/home", (data, status) => {
-        alert(data);
+    $.get("http://127.0.0.1:3000/home", (data, status) => {
+        console.log(data);
+        taskList = data.taskList;
+        // taskList.push(task);
+        $('.taskList ul').empty();
+        taskList.forEach((task)=>{
+            console.log(task)
+            taskElement = `<li>${task}<button>Delete</button></li>`;
+            $('.taskList ul').append(taskElement)
+        })
     })
-
-    // taskList.push(task);
-    // $('.taskList ul').empty();
-    // taskList.forEach((task)=>{
-    //     console.log(task)
-    //     taskElement = `<li>${task}<button>Delete</button></li>`;
-    //     $('.taskList ul').append(taskElement)
-    // })
 }
 
 
